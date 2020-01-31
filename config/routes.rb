ab26711784abf4056644187910dc24da8d109561
Mko-dai <mk0829@outlook.jp>
2020-01-31 23:34:56 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'tweets#index'
+  root 'tweets#index'      # root : トップページのアドレスを定義（localhost:3000で表示されるページ）
   resources :tweets do
     resources :comments, only: [:create]
   end
   resources :users, only: [:show]
 end
+
+#
+# resource : Railsの7大アクション
