91e54c1b5e937b6736893bb2464d675dc5015e71
Mko-dai <mk0829@outlook.jp>
2020-02-02 12:57:25 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show  #1 ユーザーマイページを表示
     user = User.find(params[:id])
     @nickname = user.nickname
-    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
+    @tweets = user.tweets.all
   end
 
 end
